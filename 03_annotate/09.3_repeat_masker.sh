b5121ecc99b41e64659e7e744ac13af7c823193f
Kara Ryan <71788965+kara-ry@users.noreply.github.com>
2025-01-28 10:38:16 -0800

    Update 09.3_repeat_masker.sh

--- 03_annotate/09.3_repeat_masker.sh
+++ 03_annotate/09.3_repeat_masker.sh
@@ -6,8 +6,25 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=20GB
 
-module load repeatmasker/4.1.5
+module load repeatmasker/4.1.5 bedtools
 
 RepeatMasker -pa 24 -s \
--lib $1 -dir ${2}_rpmask_out \
--gff ${2}.ragtag.scaffold.fasta
+-lib $1.TE -dir ${1}_rpmask \
+-gff ${1}.ragtag.scaffold.fasta
+
+# Covert repeat file to bed file for masking
+sed -e '1,3d' ${1}_rpmask.out | \
+  awk -v OFS='\t' '{print $5, $6-1, $7}' | \
+  sort -k1,1 -k2,2 -V > ${1}_rpmask.out.bed
+
+bedtools maskfasta \
+    -soft \
+    -fi ${1}.ragtag.scaffold.fasta \
+    -bed ${1}_rpmask.out.bed \
+    -fo	${1}.ragtag.scaffold.softmasked.fa
+
+bedtools maskfasta \
+    -mc X \
+     -fi ${1}.ragtag.scaffold.fasta \
+    -bed ${1}_rpmask.out.bed \
+    -fo	${1}.ragtag.scaffold.Xmasked.fa    
