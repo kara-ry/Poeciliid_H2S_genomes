1a20080d3780fd48d11e71d934c1fdd5c1278fee
Kara Ryan <71788965+kara-ry@users.noreply.github.com>
2025-05-19 11:05:33 -0700

    Create 01_map.sh

---
+++ 05_psmc/01_map.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=map # Name the job
+#SBATCH --time=2-00:00:00 # Wall clock time limit in Days-Hours:min:seconds
+#SBATCH --partition=128x24 # Run on the instruction partition
+#SBATCH --mail-user=kamaryan@ucsc.edu # Send updates to email
+#SBATCH --mail-type=ALL # send all types of updates
+#SBATCH --output=out/Map_%j.out # output file
+#SBATCH --error=err/Map_%j.err # error file
+#SBATCH --ntasks=1 # Run 1 job
+#SBATCH --ntasks-per-node=1 # One task per computer
+#SBATCH --cpus-per-task=8 # 2 CPUs per job
+#SBATCH --mem=20GB # Memory limit 20GB
+
+module load minimap2/m2-2.17 hb hb-gnu
+
+# Submit as sbatch 01_map.sh genome reads output_sam
+# -ax asm20 is for hifi reads
+
+minimap2 -ax asm20 $1 $2 > $3
