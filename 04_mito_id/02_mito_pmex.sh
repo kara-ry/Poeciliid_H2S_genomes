46275733104e68a891df4837db19046da2bf06c4
Kara Ryan <71788965+kara-ry@users.noreply.github.com>
2025-05-19 09:55:29 -0700

    Create 02_mito_pmex.sh

---
+++ 04_mito_id/02_mito_pmex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=mitohifi
+#SBATCH --output=out/mitohifi.out
+#SBATCH --error=err/mitohifi.err
+#SBATCH --time=08:00:00
+#SBATCH --mem=12G
+#SBATCH --cpus-per-task=6
+
+module load mitohifi/3.2.2
+
+# Run MitoHiFi
+mitohifi.py -r $1 -a animal \
+-f ../Pmex_mito.fa -g Pmex_mito.gb \
+-t 6
